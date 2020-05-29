168e5d64167e58f75412a9c15626889fd9f2ba91
andy-3ws <andrew.miller@3weststreet.com>
2020-05-29 15:20:23 +0100

    working deployment locally with url; dev.wildandwaggy.com

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <div>
-      Im some other page!
-      <Link to="/">Go back home</Link>
+      <p>Im some other page!</p>
+      <p><Link to="/">Go back home</Link></p>
     </div>
   );
 };
